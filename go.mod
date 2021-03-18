4e36557182279fb75b6aabcd3360f928e785ef9b
Kyle Liu <kyleliu@vvbridge.com>
2021-03-18 15:55:36 +0800

    Add a internal dhcp server.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module elinks
 
 go 1.15
+
+require (
+	github.com/coredhcp/coredhcp v0.0.0-20210317200407-00cc6002b6c9
+	github.com/insomniacslk/dhcp v0.0.0-20210315110227-c51060810aaa // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20210315160823-c6e025ad8005 // indirect
+)
--- main.go
+++ main.go
@@ -4,23 +4,121 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
 	"os/user"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/coredhcp/coredhcp/config"
+	"github.com/coredhcp/coredhcp/logger"
+	"github.com/coredhcp/coredhcp/server"
+	"github.com/sirupsen/logrus"
+
+	"github.com/coredhcp/coredhcp/plugins"
+	pl_dns "github.com/coredhcp/coredhcp/plugins/dns"
+	pl_file "github.com/coredhcp/coredhcp/plugins/file"
+	pl_leasetime "github.com/coredhcp/coredhcp/plugins/leasetime"
+	pl_nbp "github.com/coredhcp/coredhcp/plugins/nbp"
+	pl_netmask "github.com/coredhcp/coredhcp/plugins/netmask"
+	pl_prefix "github.com/coredhcp/coredhcp/plugins/prefix"
+	pl_range "github.com/coredhcp/coredhcp/plugins/range"
+	pl_router "github.com/coredhcp/coredhcp/plugins/router"
+	pl_searchdomains "github.com/coredhcp/coredhcp/plugins/searchdomains"
+	pl_serverid "github.com/coredhcp/coredhcp/plugins/serverid"
+	pl_sleep "github.com/coredhcp/coredhcp/plugins/sleep"
 )
 
-var host = flag.String("host", "", "侦听地址")
-var port = flag.String("port", "32768", "侦听端口")
-var file = flag.String("file", "TestQueue.txt", "测试队列文件名")
-var tmac = flag.String("tmac", "", "测试手机的MAC地址")
+var (
+	flagHost        = flag.String("host", "", "侦听地址")
+	flagPort        = flag.String("port", "32768", "侦听端口")
+	flagFile        = flag.String("file", "TestQueue.txt", "测试队列文件名")
+	flagTmac        = flag.String("tmac", "", "测试手机的MAC地址")
+	flagLogFile     = flag.String("logfile", "", "Name of the log file to append to. Default: stdout/stderr only")
+	flagLogNoStdout = flag.Bool("nostdout", false, "Disable logging to stdout/stderr")
+	flagLogLevel    = flag.String("loglevel", "info", fmt.Sprintf("Log level. One of %v", getLogLevels()))
+	flagConfig      = flag.String("conf", "dhcp.yml", "Use this configuration file instead of the default location")
+	flagPlugins     = flag.Bool("plugins", false, "list plugins")
+)
+
+var logLevels = map[string]func(*logrus.Logger){
+	"none":    func(l *logrus.Logger) { l.SetOutput(ioutil.Discard) },
+	"debug":   func(l *logrus.Logger) { l.SetLevel(logrus.DebugLevel) },
+	"info":    func(l *logrus.Logger) { l.SetLevel(logrus.InfoLevel) },
+	"warning": func(l *logrus.Logger) { l.SetLevel(logrus.WarnLevel) },
+	"error":   func(l *logrus.Logger) { l.SetLevel(logrus.ErrorLevel) },
+	"fatal":   func(l *logrus.Logger) { l.SetLevel(logrus.FatalLevel) },
+}
+
+func getLogLevels() []string {
+	var levels []string
+	for k := range logLevels {
+		levels = append(levels, k)
+	}
+	return levels
+}
+
+var desiredPlugins = []*plugins.Plugin{
+	&pl_dns.Plugin,
+	&pl_file.Plugin,
+	&pl_leasetime.Plugin,
+	&pl_nbp.Plugin,
+	&pl_netmask.Plugin,
+	&pl_prefix.Plugin,
+	&pl_range.Plugin,
+	&pl_router.Plugin,
+	&pl_searchdomains.Plugin,
+	&pl_serverid.Plugin,
+	&pl_sleep.Plugin,
+}
 
 func main() {
 	flag.Parse()
 
-	testMAC := strings.ToUpper(strings.Replace(*tmac, ":", "", -1))
+	// 只是输出插件信息
+	if *flagPlugins {
+		for _, p := range desiredPlugins {
+			fmt.Println(p.Name)
+		}
+		os.Exit(0)
+	}
+
+	// 设置LOG
+	log := logger.GetLogger("main")
+	fn, ok := logLevels[*flagLogLevel]
+	if !ok {
+		log.Fatalf("Invalid log level '%s'. Valid log levels are %v", *flagLogLevel, getLogLevels())
+	}
+	fn(log.Logger)
+	log.Infof("Setting log level to '%s'", *flagLogLevel)
+
+	// LOG到文件
+	if *flagLogFile != "" {
+		log.Infof("Logging to file %s", *flagLogFile)
+		logger.WithFile(log, *flagLogFile)
+	}
+	if *flagLogNoStdout {
+		log.Infof("Disabling logging to stdout/stderr")
+		logger.WithNoStdOutErr(log)
+	}
+
+	// DHCP配置文件
+	config, err := config.Load(*flagConfig)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	// 注册DHCP插件
+	for _, plugin := range desiredPlugins {
+		if err := plugins.RegisterPlugin(plugin); err != nil {
+			log.Fatalf("Failed to register plugin '%s': %v", plugin.Name, err)
+		}
+	}
+
+	// 测试手机地址必须指定
+	testMAC := strings.ToUpper(strings.Replace(*flagTmac, ":", "", -1))
 	if len(testMAC) != 12 {
 		flag.Usage()
 		LogPrintln("E", "无效的测试手机MAC地址[", testMAC, "]")
@@ -28,11 +126,17 @@ func main() {
 	}
 
 	// Init test queue
-	queue, err := CreateTestQueueFromFile(*file, testMAC)
+	queue, err := CreateTestQueueFromFile(*flagFile, testMAC)
 	if queue == nil {
 		flag.Usage()
 		LogPrintln("E", "解析TestQueue错误：", err)
 		os.Exit(1)
+	}
+
+	// Start dhcp server
+	srv, err := server.Start(config)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// Start to listen
@@ -114,18 +218,24 @@ func main() {
 		}
 	}
 	LogPrintln("[T]", "===============================================================================================")
+
+	// 关闭dhcp服务器
+	if err := srv.Wait(); err != nil {
+		log.Print(err)
+	}
+	time.Sleep(time.Second)
 }
 
 func handleListen(cli *Client) {
 	var l net.Listener
 	var err error
-	l, err = net.Listen("tcp", *host+":"+*port)
+	l, err = net.Listen("tcp", *flagHost+":"+*flagPort)
 	if err != nil {
 		LogPrintln("[E]", "Error listening:", err)
 		os.Exit(1)
 	}
 	defer l.Close()
-	LogPrintln("[I]", "Listening on "+*host+":"+*port)
+	LogPrintln("[I]", "Listening on "+*flagHost+":"+*flagPort)
 
 	for {
 		conn, err := l.Accept()
